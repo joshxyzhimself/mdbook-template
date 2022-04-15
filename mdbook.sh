2fd1687a82af9d5a7fc69f323767cc89dbf7b91b
joshxyzhimself <joshxyzhimself@gmail.com>
2022-04-16 02:56:02 +0800

    Use mdbook v0.4.18.

--- mdbook.sh
+++ mdbook.sh
@@ -1,3 +1,3 @@
-curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.17/mdbook-v0.4.17-x86_64-unknown-linux-gnu.tar.gz > ./mdbook.tar.gz
+curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.18/mdbook-v0.4.18-x86_64-unknown-linux-gnu.tar.gz > ./mdbook.tar.gz
 tar --gunzip --extract --file ./mdbook.tar.gz --directory ./ mdbook
 rm ./mdbook.tar.gz
